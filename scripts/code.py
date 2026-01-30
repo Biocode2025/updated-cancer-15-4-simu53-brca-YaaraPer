cc285476581fc83bc5b8ff0517cebdf43e447a45
YaaraPer <yaaraperlmuter@gmail.com>
2026-01-30 18:23:37 +0200

    כתבתי פונקציה שקוראת מקובץ לתוך מילון את המיפוי בין הקודונים לחומצות אמינו.

--- scripts/code.py
+++ scripts/code.py
@@ -82,6 +82,26 @@ def DNA_RNA_Cod(seq):
    
   return RNA_seq
 #------------------------------------------------
+ 
+def Read_dict(fl):
+  '''
+  הפונקציה קוראת לתוךdictionary  את המיפוי בין הקודונים לחומצות אמינו מהקובץ.
+  מקבלת: fl.
+  מחזירה: RNA_codon_table.
+  '''
+  global RNA_codon_table
+
+  for line in fl:
+    line = line.rstrip('\r\n')
+    line_list = line.split()
+   
+    codon_key = line_list[0]
+    amino_acids_value = line_list[1]
+   
+    RNA_codon_table[codon_key] = amino_acids_value
+   
+  return RNA_codon_table
+#------------------------------------------------
 
 
 
